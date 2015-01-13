cbf2fa21bb209bb06e26484d15eb4f14d3ff1c8d
Sheonna Harris <svonharris@gmail.com>
2015-01-13 17:01:10 -0500

    Overrode file ooblog.rb with current & CORRECT file

--- ooblog.rb
+++ ooblog.rb
@@ -1,17 +1,39 @@
 class Blog
-	def initialize(title, message)
-		@title = title
-		@message = message
-	end
-
-	def post
-		@time = Time.now
-		@month = @time.month
-		@date = @time.mday
-		@year = @time.year
-		puts "#{@month} #{@date}, #{@year}"
+	def front_page
+		puts "This is my blog"
+		
+		# puts "------------"
 	end
 end
 
-first_post = Blog.new "First Day of Ironhack", "Learned all about Git today."
-first_post.post
+
+# class Post
+# 	attr_reader :title, :message
+
+# 	def initialize(title, message)
+# 		@title = title
+# 		@message = message
+# 	end
+
+# 	def date
+# 		@time = Time.now
+# 		@month = @time.month
+# 		@date = @time.mday
+# 		@year = @time.year
+# 		return "#{@month} #{@date}, #{@year}"
+# 	end
+
+# 	def post_message
+# 		puts "#{@title}\n************\n#{@message}\n------------"
+# 	end
+# end
+
+
+blog = Blog.new
+blog.front_page
+# first_post = Post.new "Day 1 of Ironhack", "Learned all about Git today."
+# second_post = Post.new "Day 2 of Ironhack", "Fummabled with module Relationships."
+# third_post = Post.new "Day 3 of Ironhack", "Be better than yesterday. Hehe."
+# first_post.post_message
+
+
