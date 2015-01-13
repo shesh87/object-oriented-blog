b96e1645c61948728d67f6c10d4e2b7cbe56fb63
Sheonna Harris <svonharris@gmail.com>
2015-01-13 17:52:43 -0500

    Trying to use modules to give class A access to class B

--- ooblog.rb
+++ ooblog.rb
@@ -1,39 +1,50 @@
 class Blog
+	# include message
 	def front_page
-		puts "This is my blog"
-		
-		# puts "------------"
+		puts "#{@message}\n------------"
 	end
 end
 
 
-# class Post
-# 	attr_reader :title, :message
+class Post
+	attr_reader :title, :text, :message
 
-# 	def initialize(title, message)
-# 		@title = title
-# 		@message = message
-# 	end
+	def initialize(title, text)
+		@title = title
+		@text = text
+	end
 
-# 	def date
-# 		@time = Time.now
-# 		@month = @time.month
-# 		@date = @time.mday
-# 		@year = @time.year
-# 		return "#{@month} #{@date}, #{@year}"
-# 	end
+	def date
+		@time = Time.now
+		@month = @time.month
+		@date = @time.mday
+		@year = @time.year
+		return "#{@month} #{@date}, #{@year}"
+	end
 
-# 	def post_message
-# 		puts "#{@title}\n************\n#{@message}\n------------"
-# 	end
-# end
+	def post_message
+		@date = date()
+		@message = "#{@date}\n#{@title}\n************************\n#{@text}"
+	end
+end
+
+module message < Post
+	def send_post
+		
+	end
+end
 
 
 blog = Blog.new
 blog.front_page
-# first_post = Post.new "Day 1 of Ironhack", "Learned all about Git today."
-# second_post = Post.new "Day 2 of Ironhack", "Fummabled with module Relationships."
-# third_post = Post.new "Day 3 of Ironhack", "Be better than yesterday. Hehe."
-# first_post.post_message
+
+first_post = Post.new "Day 1 of Ironhack", "Learned all about Git today."
+second_post = Post.new "Day 2 of Ironhack", "Fummabled with module Relationships."
+third_post = Post.new "Day 3 of Ironhack", "Be better than yesterday. Hehe."
+first_post.post_message
+# second_post.post_message
+# third_post.post_message
+
+
 
 
